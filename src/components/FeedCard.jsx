fe2706703aedd790e9ff4e80ac76e2272f60e526
shivamalk-23 <k.shivamalk.22@gmail.com>
2025-02-13 11:25:13 +0530

    last commit

--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -10,16 +10,27 @@ const MovieCard = ({ list }) => {
       {list.map((data) => (
 
 
-        <Card key={data} sx={{ my:2,mx:1,width: { xs: 150,md:'200px', lg: 250 },}} >
-          <Link key={data} style={{ textDecoration: 'none',display:'flex',justifyContent:'center',alignItems:'center' }} to={`/MovieDetail/${data.id}`}>
-            <Box sx={{ m: { xs: 1, lg: 2 }, display: 'flex', flexDirection: 'column' }}>
-              <h4 style={{ margin: "10px", textDecoration: 'none' }}>
-                {data?.titleText?.text.length > 25 ? `${data?.titleText?.text.slice(0, 22)}...` : data?.titleText?.text}
-              </h4>
-              <img  style={{ margin: "10px", objectFit: 'cover' }} src={data?.primaryImage?.url} width="200px" height="300px" />
-            </Box>
-          </Link>
-        </Card>
+<Card key={data} sx={{ my: 2, mx: 1, width: { xs: 150, md: '200px', lg: 250 } }}>
+<Link key={data} style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }} to={`/MovieDetail/${data.id}`}>
+  <Box sx={{ m: { xs: 1, lg: 2 }, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <h4 style={{ margin: "10px", textDecoration: 'none', textAlign: 'center' }}>
+      {data?.titleText?.text.length > 25 ? `${data?.titleText?.text.slice(0, 22)}...` : data?.titleText?.text}
+    </h4>
+    <img
+      style={{
+        margin: "10px",
+        objectFit: 'fill', 
+        borderRadius: '8px',
+      }}
+      src={data?.primaryImage?.url}
+      alt={data?.titleText?.text} 
+      width="100%" 
+      height="auto" 
+    />
+  </Box>
+</Link>
+</Card>
+
 
       )
 
